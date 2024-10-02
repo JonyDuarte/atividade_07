aee3b77ced415ce60d7d69ddb23b02985ed38f9a
João Carlos Martins Duarte <106711748+JonyDuarte@users.noreply.github.com>
2024-10-02 20:37:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,10 @@
 - Sair do programa
 O programa deverá exibir um menu, e o usuário iré escolher a opção desejada do menu.'''
 
+import os
+import time
 # lista
 nomes = []
-import time
 
 while True:
     try:
@@ -21,6 +22,7 @@ while True:
         [5] Sair do programa \n''')
 
         opcao = int(input("Digite sua opção: "))
+        os.system("cls")
                 
         if opcao == 1:
             novo_nome = input("Informe um novo nome para lista: ")
@@ -30,30 +32,37 @@ while True:
             continue
 
         elif opcao == 2:
+             print("Lista: \n")
              for i in range(len(nomes)):
                 print(f"{i}: {nomes[i]}")
                 continue
-        
+                 
         elif opcao == 3:
+            nomes.sort()
+            print("Lista ordenada com sucesso.")
+            continue
+
+        
+        elif opcao == 4:
             indice = int(input("Informe o número  nome que deseja alterar: "))
-            confirmacao = input(f"Deseja alterar o nome {nomes[i]}? Se sim digite 'ok': ")
+            confirmacao = input(f"Deseja alterar o nome {nomes[indice]}? Se sim digite 'ok': ")
             if confirmacao == "ok":
                 novo_nome = input("Informe o novo nome: ")
                 nomes[indice] = novo_nome
                 time.sleep(0.3)
-                print(f"{novo_nome} foi adicionado com sucesso.")
-                continue
-        
-        elif opcao == 4:
-            indice = int(input("Informe o número do nome que deseja excluir: "))
-            confirmacao = input(f"Deseja excluir o nome {nomes[i]}? Se sim digite 'ok': ")
-            if confirmacao == "ok":
-                del(nomes[indice])
-                time.sleep(0.3)
-                print("Nome deletado com sucesso.")
+                print(f"{novo_nome} foi alterado com sucesso.")
                 continue
         
         elif opcao == 5:
+            indice = int(input("Informe o número do nome que deseja excluir: "))
+            confirmacao = input(f"Deseja excluir o nome {nomes[indice]}? Se sim digite 'ok': ")
+            if confirmacao == "ok":
+                del(nomes[indice])
+                time.sleep(0.3)
+                print("Nome excluído com sucesso.")
+                continue
+        
+        elif opcao == 6:
             print("Saindo do programa....")
             time.sleep(0.5)
             break
@@ -62,7 +71,7 @@ while True:
             ...        
 
     except Exception as e:
-        print("Não foi possível adicionar um novo nome. Tente novamente. {e}.")
+        print("Erro! Tente novamente. {e}.")
 
 print("Fim do programa!")
 
